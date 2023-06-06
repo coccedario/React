ac685088c9d0c4eea314c5f8378f93ef88072512
dario-cocce <dcocce@bvconline.com.ar>
2023-06-05 21:29:04 -0300

    fotod y estilos

--- front/src/componentes/layout/Footer.js
+++ front/src/componentes/layout/Footer.js
@@ -1,6 +1,15 @@
 const Footer = (props) => {
+const styles={
+
+    footer: {
+        backgroundColor: "#008b8b",
+        textAlign: "right",
+        paddingRight: "10px",
+    }
+}
+
     return (
-        <footer>
+        <footer style={styles.footer}>
             Diseñado por estudio CDC
         </footer>
 
--- front/src/paginas/Cuerdas.js
+++ front/src/paginas/Cuerdas.js
@@ -1,23 +1,95 @@
 const Cuerdas = (props) => {
+
+const styles={
+    mainnos: {
+        borderTop: "1px solid #008b8b",
+         background: "rgb(1, 0, 8)",
+        // background: rgb(1, 0, 8);
+        // background: -moz-linear-gradient(180deg, rgba(1, 0, 8, 1) 0%, rgba(0, 139, 139, 1) 66%);
+        // background: -webkit-linear-gradient(180deg, rgba(1, 0, 8, 1) 0%, rgba(0, 139, 139, 1) 66%);
+        background: "linear-gradient(180deg, rgba(1, 0, 8, 1) 0%, rgba(0, 139, 139, 1) 66%)",
+        filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr'#010008', endColorstr=#008b8b, GradientType=1)",
+       
+        paddingTop: "20px",
+        height: "auto",
+    },
+    catalogo: {
+        textAlign: "center",
+        color: "#eef5f5",
+        width: "960px",
+        margin: "0 auto",
+        textShadow: "#141414 1px",
+    },
+    
+    
+    cuerdas: {
+    
+        textAlign: "center",
+        border: "1px solid #008b8b",
+        borderRadius: "10px#",
+        width: "88%",
+        margin: "0 auto",
+        height: "260px",
+      // background: "rgb(1, 0, 8)",
+        // background: "-moz-linear-gradient(279deg, rgba(1, 0, 8, 1) 0%, rgba(4, 78, 78) 78%)",
+        // background: "-webkit-linear-gradient(279deg, rgba(1, 0, 8, 1) 0%, rgba(4, 78, 78) 78%)",
+       background: "linear-gradient(279deg, rgba(1, 0, 8, 1) 0%, rgb(4, 78, 78) 78%)",
+        filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr='#010008', endColorstr='#008b8b', GradientType=1)",
+        padding: "30px",
+        color: "#008b8b",
+        display: "flex",
+        gap: "20px",
+        justifyContent: "space-between",
+        marginTop: "20px",
+    },
+    
+    detcuerdas: {
+    
+        display: "flex",
+        flexDirection: "column",
+        color: "aliceblue",
+    },
+    
+    imgcuerdas: {
+        width: "200px",
+        height: "200px",
+        backgroundColor: "#008b8b",
+        
+    },
+    
+     img :{
+        width: "200px",
+        height: "200px",
+    
+    },
+    
+    // &:hover {
+    
+    //     transform: "scale(1.03)",
+    //     webkitTransform: "scale(1.03)",
+    //     mozTransform: "scale(1.03)",
+    // }
+}
+
     return (
 
-        <main>
+        <main >
 
-            <div className="mainnos">
+            <div style={styles.mainnos} className="mainnos">
 
-                <div className="catalogo  ">
+                <div style={styles.catalogo} className="catalogo  ">
 
                     <h3>Catalogo de cuerdas</h3>
                     <p>Tenemos todas las marcas mas reconocidas y el asesoramiento pre y post venta que necesitas</p>
 
                 </div>
                 <a href="galeria.html">
-                    <div className="cuerdas">
-                        <div className="imgcuerdas">
-                            <img src="img/acustica4.avif" height="240px" alt="" />
+                    <div style={styles.cuerdas} className="cuerdas">
+                        <div style={styles.imgcuerdas} className="imgcuerdas">
+                            <img style={styles.img} src="img/acustica4.avif" height="240px" alt="" />
 
                         </div>
-                        <div className="detcuerdas">
+                        <div style={styles.detcuerdas} className="detcuerdas">
                             <p>Guitarras Acusticas</p>
                             <p>Taylor - Fender y mas</p>
                             <p>Detalle, Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus
@@ -30,12 +102,12 @@ const Cuerdas = (props) => {
                     </div>
                 </a>
                 <a href="galeria.html">
-                    <div className="cuerdas">
-                        <div className="imgcuerdas">
-                            <img src="img/gibson3.jpeg" height="240px" alt="" />
+                    <div style={styles.cuerdas} className="cuerdas">
+                        <div  style={styles.imgcuerdas} className="imgcuerdas">
+                            <img style={styles.img} src="img/gibson3.jpeg" height="240px" alt="" />
 
                         </div>
-                        <div className="detcuerdas">
+                        <div style={styles.detcuerdas} className="detcuerdas">
                             <p>Guitarras Electricas</p>
                             <p>Gibson - Fender - Ibanez -Epiphone y otras</p>
                             <p>Detalle, Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus doloremque
@@ -47,12 +119,12 @@ const Cuerdas = (props) => {
                     </div>
                 </a>
                 <a href="galeria.html">
-                    <div className="cuerdas">
-                        <div className="imgcuerdas">
-                            <img src="img/bajo.jpg" alt="" />
+                <div style={styles.cuerdas} className="cuerdas">
+                        <div  style={styles.imgcuerdas} className="imgcuerdas">
+                            <img style={styles.img} src="img/bajo.jpg" height="240px" alt="" />
 
                         </div>
-                        <div className="detcuerdas">
+                        <div style={styles.detcuerdas} className="detcuerdas">
                             <p>Bajos electrricos y acusticos</p>
                             <p>Fender - Ibanez - Hoffner y otros</p>
                             <p>Detalle, Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus
@@ -65,12 +137,12 @@ const Cuerdas = (props) => {
                     </div>
                 </a>
                 <a href="galeria.html">
-                    <div className="cuerdas">
-                        <div className="imgcuerdas">
-                            <img src="img/violin.jpeg" height="180px" alt="" />
+                <div style={styles.cuerdas} className="cuerdas">
+                        <div  style={styles.imgcuerdas} className="imgcuerdas">
+                            <img style={styles.img} src="img/violin.jpeg" height="240px" alt="" />
 
                         </div>
-                        <div className="detcuerdas">
+                        <div style={styles.detcuerdas} className="detcuerdas">
                             <p>Violines, Violonchelos y Violas</p>
                             <p>Stradivarius y de estudio</p>
                             <p>Detalle, Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus
@@ -83,12 +155,12 @@ const Cuerdas = (props) => {
                     </div>
                 </a>
                 <a href="galeria.html">
-                    <div className="cuerdas">
-                        <div className="imgcuerdas">
-                            <img src="img/contrabajo.jpg" height="180px" alt="" />
+                <div style={styles.cuerdas} className="cuerdas">
+                        <div  style={styles.imgcuerdas} className="imgcuerdas">
+                            <img style={styles.img} src="img/contrabajo.jpg"  height="240px" alt="" />
 
                         </div>
-                        <div className="detcuerdas">
+                        <div style={styles.detcuerdas} className="detcuerdas">
                             <p>Contrabajos</p>
                             <p>Cello D - Cecilio y mas</p>
                             <p>Detalle, Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus
